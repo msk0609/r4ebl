81132a05b664250386e972ddd521e220f457c10f
mearceci <43208974+mearceci@users.noreply.github.com>
2020-05-14 21:46:21 +0900

    can select multiple control

--- R/tt_ctr.R
+++ R/tt_ctr.R
@@ -3,21 +3,24 @@
 #'Compare intensity between sample and control according to raman shift
 #'@param g.name: group name of comparison target
 #'@param var.name: variable name (Raman shift)
-#'@param ctr.name: control sp in group
+#'@param ctr.name: control sp in group/ control species can be selected more than two
 #'@return statiscal comparison of raman intensity distributionby raman shift
 #'@examples
-#'ft=ctr.tt(data = ex_rm,g.name ="Sp",var.name = "variable", ctr.name = "a0")
-#'head(ft)
+#'ft=ctr.tt(data = ex_rm,g.name ="Sp",var.name = "variable", ctr.name = c("a0","a12"))
 #'
 #'ggplot(ft, aes(x=variable, y=p,group=Comp.sp))+
 #'geom_line()+
 #'facet_wrap(.~Comp.sp, ncol = 1)
 #
 #'@export
-ctr.tt <- function(data=data, g.name,var.name, ctr.name="a0"){
+ctr.tt <- function(data=data, g.name=NULL,var.name=NULL, ctr.name=NULL){
+
 
   if (!requireNamespace("tidyverse", quietly = TRUE)) {
     stop("You need to install package tidyverse to use this function")
+  }
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    stop("You need to install package dplyr to use this function")
   }
 
   grp=as.vector(unique(data %>% select(g.name)))
@@ -25,7 +28,8 @@ ctr.tt <- function(data=data, g.name,var.name, ctr.name="a0"){
   d=data.frame()
   for (i in 1:nrow(var)) {
     temp=subset(data,data[,which(colnames(data)==var.name)]==var[i,])
-    ctemp=subset(temp,temp[,which(colnames(data)==g.name )]==ctr.name)
+    ctemp2=filter(temp, temp[,which(colnames(data)==g.name )] %in% ctr.name)
+    head(ctemp2)
     for (j in 1:nrow(grp)) {
       ttemp=subset(temp,temp[,which(colnames(data)==g.name)]==grp[j,])
       tt=t.test(ctemp$value, ttemp$value, var.equal = T)
